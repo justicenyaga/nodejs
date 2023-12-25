f67b32a8b0d913ca875372afd79faaa1d8fb9462
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-25 16:08:20 +0300

    Extending EventEmitter

--- first-app/app.js
+++ first-app/app.js
@@ -1,11 +1,9 @@
-const EventEmitter = require("events");
-
-const emitter = new EventEmitter();
+const Logger = require("./logger");
+const logger = new Logger();
 
 // Register a listener
-emitter.on("messageLogged", (arg) => {
+logger.on("messageLogged", (arg) => {
   console.log("Listener called", arg);
 });
 
-// Raise an event
-emitter.emit("messageLogged", { id: 1, url: "http:/blah" });
+logger.log("message");

ad0ea6bebddaabe8b0ae3e1e35943f3ef5fc708d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-25 14:34:43 +0300

    Working with OS module

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,7 @@
-const path = require("path");
+const os = require("os");
 
-var pathObj = path.parse(__filename);
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
 
-console.log(pathObj);
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);

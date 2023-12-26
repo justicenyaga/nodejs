4caeff3698c665e2fd035b84ccacfdc1ab6d21b8
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-27 00:52:20 +0300

    Add a route to delete a genre

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -36,6 +36,16 @@ router.put("/:id", (req, res) => {
   res.send(genre);
 });
 
+router.delete("/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
+
 router.get("/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genre)

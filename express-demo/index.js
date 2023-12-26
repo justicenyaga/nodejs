b7f4bb0f9fb248f3b5ec2761e1e9487ec3f3a36b
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-26 10:52:49 +0300

    Using built-in middlewares

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
-const authenticator = require("./authenticator");
-const logger = require("./logger");
 const express = require("express");
+const logger = require("./logger");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(logger);
-app.use(authenticator);
 
 const courses = [
   { id: 1, name: "course1" },

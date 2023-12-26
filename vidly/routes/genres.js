a9375c716fd7a7f1d7c50613a6b2e031f4e28ae8
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-27 00:51:46 +0300

    Add a route to update a genre

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -24,6 +24,18 @@ router.post("/", (req, res) => {
   res.send(genre);
 });
 
+router.put("/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
 router.get("/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genre)

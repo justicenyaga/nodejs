62e78c58084b5256b13f8559c9916f633d002287
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-26 23:22:15 +0300

    Debugging

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 const express = require("express");
 const logger = require("./logger");
 const config = require("config");
+const debug = require("debug")("app:startup");
 
 const app = express();
 
@@ -12,13 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
 
-console.log("App Name: ", config.get("name"));
-console.log("Mail Server: ", config.get("mail.host"));
-console.log("Mail Password: ", config.get("mail.password"));
-
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  console.log("Morgan enabled.");
+  debug("Morgan enabled.");
 }
 
 app.use(logger);

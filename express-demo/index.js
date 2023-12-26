62d9642d756fcc803b8d29d2267f02bcdea70067
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-26 10:14:38 +0300

    Handling DELETE request

--- express-demo/index.js
+++ express-demo/index.js
@@ -42,6 +42,17 @@ app.put("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)

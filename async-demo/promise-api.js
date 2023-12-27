f18535e0703dcfddf4b8d34eb9d2c3811da76f7d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-27 12:50:48 +0300

    Running promises in parallel

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,7 +1,28 @@
-// Creating a resolved promise
-const resolved_p = Promise.resolve({ id: 1 });
-resolved_p.then((result) => console.log(result));
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+  }, 3000);
+});
 
-// Creating a rejected promise
-const rejected_p = Promise.reject(new Error("Reason for rejection..."));
-rejected_p.catch((error) => console.log("Error: ", error.message));
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    reject(new Error("Because something failed"));
+  }, 2000);
+});
+
+Promise.all([p1, p2]).then((result) => console.log("All resolved: ", result));
+Promise.all([p1, p3])
+  .then((result) => console.log(result))
+  .catch((error) => console.log("At least one error: ", error.message));
+Promise.race([p1, p2]).then((result) =>
+  console.log("The first to be resolved: ", result)
+);

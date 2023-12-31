a3fdb084962b879b03100d02c366a2cc754a252b
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-31 15:05:48 +0300

    Extracting the validation logic

--- vidly/index.js
+++ vidly/index.js
@@ -1,6 +1,4 @@
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
+const winston = require("winston");
 const express = require("express");
 const app = express();
 
@@ -8,8 +6,9 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  winston.info(`Listening on port ${port}...`);
 });

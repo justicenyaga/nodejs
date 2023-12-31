f1b4c2dd28cf8c3477fbf769b0e3afa0183f634b
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-31 14:58:17 +0300

    Extracting the logging logic

--- vidly/index.js
+++ vidly/index.js
@@ -1,6 +1,3 @@
-require("express-async-errors");
-const winston = require("winston");
-require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
@@ -8,22 +5,9 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-// winston.add(winston.transports.MongoDB, {
-//   db: "mongodb://localhost/vidly",
-//   level: "info",
-// });
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey not defined");

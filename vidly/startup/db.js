0660a91b403a8ace329fd269a2c861bbf16c2d9a
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-06 18:23:05 +0300

    Setting up the test DB

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
+const config = require("config");
 
 module.exports = function () {
+  const db = config.get("db");
   mongoose
-    .connect("mongodb://localhost/vidly", {
+    .connect(db, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
     })
-    .then(() => winston.info("Connected to MongoDB..."));
+    .then(() => winston.info(`Connected to ${db}...`));
 };

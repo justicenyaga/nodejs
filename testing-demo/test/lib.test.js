9935ed3cfe89205adaa818892b68dd3156dcad3c
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-06 11:04:24 +0300

    Testing exceptions

--- testing-demo/test/lib.test.js
+++ testing-demo/test/lib.test.js
@@ -59,3 +59,20 @@ describe("getProduct", () => {
     expect(result).toHaveProperty("id", 1);
   });
 });
+
+describe("registerUser", () => {
+  it("should throw if username if falsy", () => {
+    const args = [null, undefined, NaN, "", 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+
+  it("should return a user object if a valid username is passed", () => {
+    const result = lib.registerUser("justice");
+    expect(result).toMatchObject({ username: "justice" });
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
